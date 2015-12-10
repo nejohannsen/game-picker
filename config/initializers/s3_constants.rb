51543b5ac2110ee7d5dfac761ebbab1b431b3dce
Nick <nicholas.johannsen@gmail.com>
2015-12-10 08:41:54 -0700

    Update s3_constants.rb

--- config/initializers/s3_constants.rb
+++ config/initializers/s3_constants.rb
@@ -2,6 +2,6 @@ Paperclip::Attachment.default_options[:url] = ':s3-us-west-2'
 if Rails.env.development? || Rails.env.test?
   ENV['S3_HOST_NAME'] = "s3-us-west-2"
   ENV['S3_KEY'] = "AKIAJFDPL64FRKVDHWJQ"
-  ENV['S3_SECRET'] = "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"
+  ENV['S3_SECRET'] = ""
   ENV['S3_BUCKET'] = 'game-picker-dev'
 end
